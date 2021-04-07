5878c8dc351504c1385706f5e7ed38d978f6c007
DeVriesMatt <matthew.devries.13@gmail.com>
2021-04-07 16:49:30 +0100

    update run_conv to test for different number of features

--- run_conv.sh
+++ run_conv.sh
@@ -5,13 +5,16 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=6G
-#SBATCH --time=24:00:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/output.out
-#SBATCH --error=/home/mvries/DeepClusterConv/error_file/error.err
+#SBATCH --time=72:00:00
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/%joutput.out
+#SBATCH --error=/home/mvries/DeepClusterConv/error_file/%jerror.err
 #SBATCH --partition=gpu
 module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 
 conda activate dcfn
 
-python main.py  --output_dir '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/' --dataset_path '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/OPM_Roi_Images_Full_646464_Cluster3/OPM_Roi_Images_Full_646464_Cluster3'
+for num_features in 10 50 100 200 512
+do
+  python main.py  --epochs 1000 --epochs_pretrain 300 --num_features $num_features --output_dir '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/' --dataset_path '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/Datasets/ModelNet10Voxel/Train/' --update_interval 1
+done
