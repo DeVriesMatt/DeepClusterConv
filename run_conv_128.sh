0447985a9727a0d09eff80dd4b8652f2a453db06
DeVriesMatt <matthew.devries.13@gmail.com>
2021-05-24 16:33:58 +0100

    enable resnet

--- run_conv_128.sh
+++ run_conv_128.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=6G
-#SBATCH --time=250:00:00
+#SBATCH --time=90:00:00
 #SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/%joutput.out
 #SBATCH --error=/home/mvries/DeepClusterConv/error_file/%jerror.err
 #SBATCH --partition=gpu
@@ -16,5 +16,5 @@ conda activate dcfn
 
 for num_features in 10 50 100 200 512
 do
-    python main.py  --epochs 500 --epochs_pretrain 300 --num_features $num_features --output_dir '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/' --dataset_path '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/Datasets/SingleCell_ERK_Cell_128/' --custom_img_size [128,128,128,1] --update_interval 1 --gamma 0.999 --dataset 'SinglecellERK_128'
+    python main.py  --net_architecture 'CAE_bn3_Seq' --epochs 500 --epochs_pretrain 200 --num_features $num_features --output_dir '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/DeepClusterConv/' --dataset_path '/data/scratch/DBI/DUDBI/DYNCESYS/mvries/Datasets/SingleCell_ERK_Cell_128/' --custom_img_size [128,128,128,1] --update_interval 1 --gamma 0.999 --dataset 'SinglecellERK_128'
 done
